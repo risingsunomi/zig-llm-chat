6fedd80982f9340998e104650a243bfd3b0605ff
risingsunomi <vincentcastro@gmail.com>
2024-07-06 03:47:47 -0400

    finally got windows.h to work, removed capy for now, created win_msgbox.zip for windows api MessageBoxA interface

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,11 @@
-const std = @import("std");
+// building out gui
+// first focus is windows.h
+const winh = @cImport(@cInclude("windows.h"));
+const win_mbx = @import("win_msgbox.zig");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn main() anyerror!void {
+    const message = "Hello from Windows API!";
+    const title = "Testing win_msgbox.zig";
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    try win_mbx.winMsgBox(message, title);
 }
